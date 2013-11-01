8b5033d769741dcf78cec93d6f84e3da7b7fa3df
Christoph Scheid <c@shri.de>
2013-11-01 18:02:36 +0100

    update ci.sh for ci

--- ci.sh
+++ ci.sh
@@ -11,7 +11,19 @@ env/bin/pip install --upgrade setuptools
 # set it up
 # this will take a little while and install all necessary dependencies.
 env/bin/python setup.py develop
+# populate the database
+env/bin/initialize_c3sMembership_db development.ini
+# prepare for tests
+env/bin/pip install nose coverage pep8 pylint pyflakes
 # run the tests
-env/bin/nosetests c3smembership/
+env/bin/nosetests c3smembership/   --with-coverage --cover-html --with-xunit
 # this is how you can run individial tests:
 #env/bin/nosetests c3smembership/tests/test_webtest.py:FunctionalTests.test_faq_template
+
+# for pyflakes
+find c3smembership -regex '.*.py' ! -regex '.*tests.*'|egrep -v '^./tests/'|xargs env/bin/pyflakes  > pyflakes.log || :
+# for pylint
+rm -f pylint.log
+for f in `find c3smembership -regex '.*.py' ! -regex '.*tests.*'|egrep -v '^./tests/'`; do
+env/bin/pylint --output-format=parseable --reports=y $f >> pylint.log
+done || :
